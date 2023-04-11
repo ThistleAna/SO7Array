9f11529aeb2145fe58a449a7ef1e9b0aa666eef5
ANA KHASANAH <ana.khasanah@capita.com>
2023-04-11 12:47:13 +0100

    Array

---
+++ src/ReverseString.java
@@ -0,0 +1,17 @@
+// 1. import the scanner
+
+public class ReverseString {
+    public static void main(String[] args) {
+        System.out.println("This program will reverse any sentence");
+        //2. create scanner object
+
+        // 3. ask user to enter a name or sentence to reverse
+
+        // 4. read user input as char array
+        char[] letters = reader.nextLine().toCharArray();
+        // 5. display the sentence using for loop
+
+        // 6. print in reverse, hint: start from .length to index 0
+        System.out.println("The reversed sentence is : ");
+    }
+}
